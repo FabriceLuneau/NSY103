e185581144ba6c62f8b9e46d8ffd94adf41b49b8
Fabrice LUNEAU <fabrice.luneau76@gmail.com>
2025-12-26 05:07:56 +0100

    ajout fonctions creer spectacle et ajouter spectacle au tableau

---
+++ testSpectacle.c
@@ -0,0 +1,24 @@
+#include <stdbool.h>
+#include<stdio.h>
+#include"spectacle.h"
+
+
+int main(int argc, char** argv) 
+{
+	int id;
+char intitule[50];
+int nbPlaces;
+
+	//printf("Entrer l'id du spectacle \n");
+	//scanf("%i", id);
+	
+	printf("Entrer l'intitule du   spectacle \n");
+	scanf("%s", intitule);
+	
+	printf("Entrer le nombre de place du spectacle \n");
+	scanf("%i", nbPlaces);
+	
+	
+	return 0;
+	}
+ 
