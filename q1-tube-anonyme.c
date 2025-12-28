694a2e46bbf6a257bae1f3dbcb532d9179313cb7
Fabrice luneau <luneau@debian.monreseau>
2025-12-28 12:02:48 +0100

    q1 ajout dela gestion du fichier  log  pour le serveur

--- q1-tube-anonyme.c
+++ q1-tube-anonyme.c
@@ -1,177 +1,167 @@
 #include <stdbool.h>
-#include<stdio.h>
-#include"spectacle.h"
+#include <stdio.h>
+#include "spectacle.h"
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include"request.h";
-#include"response";
-
+#include <fcntl.h>
+#include "request.h"
+#include "response.h"
 
 /*
 Version question 1 avec tubes anonymes
 */
-int main(int argc, char** argv) 
+
+int main(int argc, char** argv)
 {
-	//initialisation des données, le tableau est dans le header spectacle
-	//les, deux processus auront leur copie et se synchroniseront ensuite
-	init();
+    // initialisation des données
+    init();
 
-	//déclaration des tubes on se place du cote serveur pourles noms
-int outcoming[2];
-pipe(outcoming);
+    // déclaration des tubes
+    int outcoming[2];
+    if (pipe(outcoming) == -1) {
+        perror("pipe outcoming failed");
+        return errno;
+    }
 
-	   if(outcoming == -1)
-   {
-         perror("pipe creation failled");
-         return errno;
-   } 
-	
-	int incoming[2];
-	pipe(incoming);
+    int incoming[2];
+    if (pipe(incoming) == -1) {
+        perror("pipe incoming failed");
+        return errno;
+    }
 
-	if(incoming == -1)
-   {
-         perror("pipe creation failled");
-         return errno;
-   }
-		
-	//creation du processus enfant
-	pid_t res = fork();
+    // création du processus enfant
+    pid_t res = fork();
+    if (res == -1) {
+        perror("fork failed");
+        return errno;
+    }
 
-	   if(res == -1)
-   {
-         perror("fork failled");
-         return errno;
-   }
-	
-	//division en processus clieent et enfan 
-	if(res == 0) 
-	{
-		printf("Lancement du processusc client %i \n", getpid());
+    /* ===================== CLIENT ===================== */
+    if (res == 0) {
 
-		//fermeture des descripteurs inutiles
-		        close(incoming[0]);
-        close(outcoming[1]);
+        printf("Lancement du processus client PID=%d\n", getpid());
 
-		
-	//valeur impossible de choix  pour le démarage
-	int specSelect = 9999;
-	
-	//Valeur d'action 99 impossible pour rester dans la boucle while
-			int choix = 99;
-			
-				//Tant que l'utilisateur ne choisit pas 0 on reste dans la boucle
-				while(choix != 0) {
-        struct request req; 
-					
-			printf("Menu \n\n");
-			
-						//Il faut choisir un spectacle pour avoir accès aux opérations, il est affiché en rappel, 9999 correspond à l'absence de choix
-			if(specSelect != 9999) 
-			{
-				printf("Spectacle selectionne: %i %s %i \n", specSelect, tabSpectacles[specSelect].intitule, tabSpectacles[specSelect].nbPlaces);
-				}  
-				else 
-				{
-							printf("Pas de spectacles selectionne \n");
-				}
-							
-										//operations toujours disponibles
-										printf("1 selectionner un spectacle \n");
-										printf("2 Afficher les spectacles \n");
-										
-										//operations disponibles seulement avec un spectacle selectionne
-										if(specSelect != 9999) 
-										{
-				printf("3 Retirer des places \n");
-				printf("4 Ajouter des  places \n");
-				printf("\n 0 Quitter \n\n");
-				}
-				
-				printf("Entrez votre choix \n");
-				scanf("%d", &choix);
-				
-				printf("Votre choix %i \n", choix);
-				
-				//traitments des choix
-				switch(choix) 
-					{
-					case 1:
-					int idSpectacle;
-					
-					printf("Entrez l'id du spectacle \n");
-				scanf("%d", &idSpectacle);
-					specSelect = idSpectacle;
-					
-					 					 break;
-					 case 2:
-					 afficher();
-					 
-					 break;
-					 case 3:
-					 if(specSelect != 9999) 
-					 {
-						 int nbPlaces;
-						 
-						 printf("Entrez le nombbre de places \n");
-				scanf("%d", &nbPlaces);
+        // fermeture des descripteurs inutiles
+        close(incoming[0]);   // lecture serveur
+        close(outcoming[1]);  // écriture serveur
 
-						 req.idSpectacle) = specSelect; 
-req.nbPlaces = nbPlaces;
+        int specSelect = 9999;
+        int choix = 99;
 
-        bool resultat; 
+        while (choix != 0) {
 
-        write(incoming[1], &req, sizeof(req));
-        read(outcoming[0], &resultat, sizeof(resultat)); 
-						 
-        if (resultat) {  
-						 {
-	printf("places retirees \n");
-					 }
-					 else 
-					 {
-						 printf("Places insufisantepour la demande s\n");
-					 }
-					 }
-					 break;
-					 case 0:
-close(incoming[0]);
-	close(outcoming[1])
-	
-					 return 0;
-					 break;
-					 default:
-					 printf("Operation non supportee\n");
-					 break;
-				}
+            struct request req;
 
-	}
-	}
-	else
-	{
-		//partie serveur
-		
-//fermeture des descripteurs intutiles
-		close(incoming[1]);
-        close(outcoming[0]);
+            printf("\nMenu\n");
+            if (specSelect != 9999) {
+                printf("Spectacle selectionne: %d %s (%d places)\n",
+                       specSelect,
+                       tabSpectacles[specSelect].intitule,
+                       tabSpectacles[specSelect].nbPlaces);
+            } else {
+                printf("Pas de spectacle selectionne\n");
+            }
 
-		while(1) 
-		{
-			        struct request req;
-			
-        read(incoming[0], &req, sizeof(req));
+            printf("1 Selectionner un spectacle\n");
+            printf("2 Afficher les spectacles\n");
+            if (specSelect != 9999) {
+                printf("3 Retirer des places\n");
+            }
+            printf("0 Quitter\n");
 
-			        bool ok = retirerPlaces(req.idSpectacle, req.nbPlaces);
+            printf("Entrez votre choix: ");
+            scanf("%d", &choix);
 
-			        write(outcoming[1], &ok, sizeof(ok)); 
-		}
+            switch (choix) {
 
-//section inataignable le client quite dans sa partie 
-		//et le serveurdoit être coupe avec ctrl + c, les ressources seront libéré par le system  
-	}
-			
-	return 0;
-	}
- 
+                case 1: {
+                    int id;
+                    printf("Entrez l'id du spectacle: ");
+                    scanf("%d", &id);
+                    specSelect = id;
+                    break;
+                }
+
+                case 2:
+                    afficher();
+                    break;
+
+                case 3:
+                    if (specSelect != 9999) {
+                        int nbPlaces;
+                        printf("Entrez le nombre de places: ");
+                        scanf("%d", &nbPlaces);
+
+                        req.idSpectacle = specSelect;
+                        req.nbPlaces = nbPlaces;
+
+                        write(incoming[1], &req, sizeof(req));
+
+                        bool resultat;
+                        read(outcoming[0], &resultat, sizeof(resultat));
+
+                        if (resultat) {
+                            printf("Reservation effectuee avec succes\n");
+                            retirerPlaces(specSelect, nbPlaces); // synchro locale
+                        } else {
+                            printf("Echec: places insuffisantes\n");
+                        }
+                    }
+                    break;
+
+                case 0:
+                    close(incoming[1]);
+                    close(outcoming[0]);
+                    return 0;
+
+                default:
+                    printf("Operation non supportee\n");
+            }
+        }
+    }
+
+    /* ===================== SERVEUR ===================== */
+    else {
+
+        // redirection sortie serveur vers server.log
+        int logfd = open("server.log", O_WRONLY | O_CREAT | O_APPEND, 0644);
+        if (logfd == -1) {
+            perror("open server.log failed");
+            exit(EXIT_FAILURE);
+        }
+        dup2(logfd, STDOUT_FILENO);
+        dup2(logfd, STDERR_FILENO);
+        close(logfd);
+
+        printf("Serveur demarre PID=%d\n", getpid());
+
+        // fermeture des descripteurs inutiles
+        close(incoming[1]);   // écriture client
+        close(outcoming[0]);  // lecture client
+
+        while (1) {
+
+            struct request req;
+            ssize_t n = read(incoming[0], &req, sizeof(req));
+
+            if (n <= 0) {
+                printf("Client deconnecte, arret du serveur\n");
+                break;
+            }
+
+            printf("Requete recue: spectacle=%d, nbPlaces=%d\n",
+                   req.idSpectacle, req.nbPlaces);
+
+            bool ok = retirerPlaces(req.idSpectacle, req.nbPlaces);
+
+            printf("Resultat traitement: %s\n", ok ? "SUCCESS" : "FAIL");
+
+            write(outcoming[1], &ok, sizeof(ok));
+        }
+    }
+
+    return 0;
+}
+
