276b6320810eed0ed3593bf6a8dc1629f6a5f684
Fabrice LUNEAU <fabrice.luneau76@gmail.com>
2025-12-30 05:54:34 +0100

    ajout de la doc relue, correction de la gestion des fifo pour la version avec tubes nommes

--- q1b-client-tube-nomme.c
+++ q1b-client-tube-nomme.c
@@ -1,128 +1,105 @@
-#include <stdbool.h>
 #include <stdio.h>
-#include "spectacle.h"
-#include <errno.h>
-#include <string.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <errno.h>
+
+#include "spectacle.h"
 #include "request.h"
 #include "response.h"
-
-/*
-Version client avec tubes nommés (FIFO)
-*/
-
-#define FIFO_IN  "incoming.fifo"
-#define FIFO_OUT "outcoming.fifo"
+#include "fifo.h"
 
 int main(void)
 {
     init();
 
-    /* création des FIFO si absents */
-    if (mkfifo(FIFO_IN, 0666) == -1 && errno != EEXIST) {
-        perror("mkfifo incoming");
-        exit(EXIT_FAILURE);
-    }
-
-    if (mkfifo(FIFO_OUT, 0666) == -1 && errno != EEXIST) {
-        perror("mkfifo outcoming");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Client demarre PID=%d\n", getpid());
-
-    /* ouverture des FIFO */
-    int fd_out = open(FIFO_IN, O_WRONLY);
-    if (fd_out == -1) {
-        perror("open FIFO_IN");
-        exit(EXIT_FAILURE);
-    }
-
-    int fd_in = open(FIFO_OUT, O_RDONLY);
-    if (fd_in == -1) {
-        perror("open FIFO_OUT");
-        exit(EXIT_FAILURE);
-    }
+    printf("Client FIFO démarré (PID=%d)\n", getpid());
 
     int specSelect = -1;
-    int choix = 99;
+    int choix = -1;
 
     while (choix != 0) {
-        struct request req;
-        struct response resp;
 
         printf("\nMenu\n");
-
         if (specSelect >= 0) {
-            printf("Spectacle selectionne: %d %s (%d places)\n",
+            printf("Spectacle sélectionné : %d %s (%d places)\n",
                    specSelect,
                    tabSpectacles[specSelect].intitule,
                    tabSpectacles[specSelect].nbPlaces);
         } else {
-            printf("Pas de spectacle selectionne\n");
+            printf("Aucun spectacle sélectionné\n");
         }
 
-        printf("1 Selectionner un spectacle\n");
-        printf("2 Afficher les spectacles\n");
+        printf("1 - Sélectionner un spectacle\n");
+        printf("2 - Afficher les spectacles\n");
         if (specSelect >= 0) {
-            printf("3 Retirer des places\n");
+            printf("3 - Réserver des places\n");
         }
-        printf("0 Quitter\n");
-
-        printf("Choix: ");
+        printf("0 - Quitter\n");
+        printf("Choix : ");
         scanf("%d", &choix);
 
         switch (choix) {
 
-            case 1: {
-                printf("Id du spectacle: ");
-                scanf("%d", &specSelect);
-                break;
-            }
+        case 1:
+            printf("Id du spectacle : ");
+            scanf("%d", &specSelect);
+            break;
 
-            case 2:
-                afficher();
-                break;
+        case 2:
+            afficher();
+            break;
 
-            case 3:
-                if (specSelect >= 0) {
-                    int nb;
-                    printf("Nombre de places: ");
-                    scanf("%d", &nb);
+        case 3:
+            if (specSelect >= 0) {
 
-                    req.idSpectacle = specSelect;
-                    req.nbPlaces = nb;
+                struct request req;
+                int nbPlaces;
 
-                    if (write(fd_out, &req, sizeof(req)) != sizeof(req)) {
-                        perror("write request");
-                        break;
-                    }
+                printf("Nombre de places : ");
+                scanf("%d", &nbPlaces);
 
-                    if (read(fd_in, &resp, sizeof(resp)) != sizeof(resp)) {
-                        perror("read response");
-                        break;
-                    }
+                req.idSpectacle = specSelect;
+                req.nbPlaces = nbPlaces;
 
-                    if (resp.success) {
-                        printf("Reservation OK\n");
-                        retirerPlaces(specSelect, nb); // synchro locale
-                    } else {
-                        printf("Echec: places insuffisantes ou id invalide\n");
-                    }
+                /* Envoi requête */
+                int fd_out = open(FIFO_IN, O_WRONLY);
+                if (fd_out == -1) {
+                    perror("open FIFO_IN");
+                    break;
                 }
-                break;
 
-            case 0:
-                printf("Client termine\n");
+                write(fd_out, &req, sizeof(req));
                 close(fd_out);
-                close(fd_in);
-                return 0;
 
-            default:
-                printf("Choix invalide\n");
+                /* Lecture réponse */
+                bool resultat;
+                int fd_in = open(FIFO_OUT, O_RDONLY);
+                if (fd_in == -1) {
+                    perror("open FIFO_OUT");
+                    break;
+                }
+
+                read(fd_in, &resultat, sizeof(resultat));
+                close(fd_in);
+
+                if (resultat) {
+                    printf("Réservation réussie\n");
+                    retirerPlaces(specSelect, nbPlaces); // synchro locale
+                } else {
+                    printf("Échec : places insuffisantes\n");
+                }
+            }
+            break;
+
+        case 0:
+            printf("Fin du client\n");
+            break;
+
+        default:
+            printf("Choix invalide\n");
         }
     }
 
--- q1b-server-tube-nomme.c
+++ q1b-server-tube-nomme.c
@@ -1,117 +1,69 @@
-#include <sys/stat.h>
-#include <stdbool.h>
 #include <stdio.h>
-#include <errno.h>
-#include <string.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <errno.h>
 
 #include "spectacle.h"
 #include "request.h"
 #include "response.h"
-
-#define FIFO_IN  "incoming.fifo"
-#define FIFO_OUT "outcoming.fifo"
-
-/* fichier de log */
-static FILE *log_file = NULL;
-
-/* log vers fichier + ecran */
-void log_msg(const char *fmt, ...)
-{
-    va_list args;
-
-    va_start(args, fmt);
-    vprintf(fmt, args);
-    va_end(args);
-
-    if (log_file) {
-        va_start(args, fmt);
-        vfprintf(log_file, fmt, args);
-        fflush(log_file);
-        va_end(args);
-    }
-}
+#include "fifo.h"
 
 int main(void)
 {
     init();
 
-    /* ouverture du fichier de log */
-    log_file = fopen("server.log", "a");
-    if (!log_file) {
-        perror("fopen server.log");
-        exit(EXIT_FAILURE);
-    }
-
-    /* creation des FIFO (si inexistants) */
+    /* Création des FIFO (si inexistantes) */
     if (mkfifo(FIFO_IN, 0666) == -1 && errno != EEXIST) {
-        perror("mkfifo incoming");
+        perror("mkfifo fifo_in");
         exit(EXIT_FAILURE);
     }
 
     if (mkfifo(FIFO_OUT, 0666) == -1 && errno != EEXIST) {
-        perror("mkfifo outcoming");
+        perror("mkfifo fifo_out");
         exit(EXIT_FAILURE);
     }
 
-    log_msg("Serveur demarre PID=%d\n", getpid());
-
-    /* ouverture des FIFO */
-    int fd_in = open(FIFO_IN, O_RDONLY);
-    if (fd_in == -1) {
-        perror("open incoming");
-        exit(EXIT_FAILURE);
-    }
-
-    int fd_out = open(FIFO_OUT, O_WRONLY);
-    if (fd_out == -1) {
-        perror("open outcoming");
-        exit(EXIT_FAILURE);
-    }
+    printf("Serveur FIFO démarré (PID=%d)\n", getpid());
 
     while (1) {
-        struct request req;
-        ssize_t n = read(fd_in, &req, sizeof(req));
 
-        if (n == 0) {
-            log_msg("Client deconnecte\n");
-            break;
-        }
-
-        if (n != sizeof(req)) {
-            log_msg("Requete incomplete recue\n");
+        /* 1. Attente requête client */
+        int fd_in = open(FIFO_IN, O_RDONLY);
+        if (fd_in == -1) {
+            perror("open FIFO_IN");
             continue;
         }
 
-        log_msg("Requete: id=%d nbPlaces=%d\n",
-                req.idSpectacle, req.nbPlaces);
+        struct request req;
+        ssize_t n = read(fd_in, &req, sizeof(req));
+        close(fd_in);
 
-        struct response resp;
-
-        if (req.idSpectacle < 0 || req.idSpectacle >= nbSpectacles) {
-            resp.code = 404;
-        } else if (!retirerPlaces(req.idSpectacle, req.nbPlaces)) {
-            resp.code = 409;
-        } else {
-            resp.code = 200;
+        if (n != sizeof(req)) {
+            printf("Client déconnecté\n");
+            continue;
         }
 
-        log_msg("Reponse envoyee: code=%d\n", resp.code);
+        printf("Requête reçue : spectacle=%d, nbPlaces=%d\n",
+               req.idSpectacle, req.nbPlaces);
 
-        if (write(fd_out, &resp, sizeof(resp)) != sizeof(resp)) {
-            perror("write response");
-            break;
+        /* 2. Traitement */
+        bool ok = retirerPlaces(req.idSpectacle, req.nbPlaces);
+
+        printf("Résultat : %s\n", ok ? "SUCCESS" : "FAIL");
+
+        /* 3. Réponse au client */
+        int fd_out = open(FIFO_OUT, O_WRONLY);
+        if (fd_out == -1) {
+            perror("open FIFO_OUT");
+            continue;
         }
+
+        write(fd_out, &ok, sizeof(ok));
+        close(fd_out);
     }
-
-    close(fd_in);
-    close(fd_out);
-    fclose(log_file);
-
-    unlink(FIFO_IN);
-    unlink(FIFO_OUT);
 
     return 0;
 }
