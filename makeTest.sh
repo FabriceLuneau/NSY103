d1b2eff48ab21f073080c188cd86e21bb1bc25bc
Fabrice LUNEAU <fabrice.luneau76@gmail.com>
2026-01-19 12:19:21 +0100

    correction du script de compilation des tests

--- makeTest.sh
+++ makeTest.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/bash
 
-#Supprime les build précédents
+#Supprime les test précédents
 #on utilise .exe pour mieux discriminer  les exécutables et avoir une règle dans le git ignore
 rm test/*.exe
 
 #compilation
 
 #test des fonctions spectacles
-gcc testSpectacle.c -o build/spectacle.exe
+gcc testSpectacle.c -o test/spectacle.exe
 #test descle valeur
-gcc testCleValeur.c -o build/cleValeur.exe
+gcc testClefValeur.c -o test/cleValeur.exe
 
 #teste dus protocoles de communication
-gcc testRequest.c  -o build/request.exe
-gcc testResponse.c -o build/response.exe
+gcc testRequest.c  -o test/request.exe
+gcc testResponse.c -o test/response.exe
 
 #test du bridge de donnees
-gcc testBridge.c -o build/bridge.exe
+gcc testBridge.c -o test/bridge.exe
 
